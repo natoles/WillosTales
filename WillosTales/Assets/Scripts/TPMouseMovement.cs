a1d48da770bc444af418259cd48bd6ee24a87ffd
natoles <lindien.martin@gmail.com>
2019-10-21 14:10:21 -0400

    added TPmovement, TPMouseMovement is WIP

---
+++ WillosTales/Assets/Scripts/TPMouseMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPMouseMovement : MonoBehaviour
+{
+    public GameObject playerCamera;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    float rotationY = 0F;
+    void Update()
+    {
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        playerCamera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        transform.localEulerAngles = new Vector3(0, rotationX, 0);
+    }
+
+    void Start()
+    {
+        // Make the Rigidbody not change rotation
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        if (rigidbody)
+            rigidbody.freezeRotation = true;
+    }
+}
