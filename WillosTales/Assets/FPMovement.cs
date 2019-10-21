3725f680cb549379efe3f3f006df00b01a5c19fa
natoles <lindien.martin@gmail.com>
2019-10-21 13:22:05 -0400

    added air control in fps view

--- WillosTales/Assets/FPMovement.cs
+++ WillosTales/Assets/FPMovement.cs
@@ -21,32 +21,36 @@ public class FPMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        
+
+        // Calculate how fast we should be moving
+        Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity *= speed;
+
+        // Apply a force that attempts to reach our target velocity
+        Vector3 velocity = playerRb.velocity;
+        Vector3 velocityChange = (targetVelocity - velocity);
+        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+        velocityChange.y = 0;
+        playerRb.AddForce(velocityChange, ForceMode.VelocityChange);
+
+        // We apply gravity manually for more tuning control
+        playerRb.AddForce(new Vector3(0, -gravity * playerRb.mass, 0));
+
+        
+
         if (grounded)
         {
-            // Calculate how fast we should be moving
-            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            targetVelocity = transform.TransformDirection(targetVelocity);
-            targetVelocity *= speed;
-
-            // Apply a force that attempts to reach our target velocity
-            Vector3 velocity = playerRb.velocity;
-            Vector3 velocityChange = (targetVelocity - velocity);
-            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-            velocityChange.y = 0;
-            playerRb.AddForce(velocityChange, ForceMode.VelocityChange);
-
             // Jump
             if (canJump && Input.GetButton("Jump"))
             {
                 playerRb.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
             }
+
+            grounded = false;
         }
-
-        // We apply gravity manually for more tuning control
-        playerRb.AddForce(new Vector3(0, -gravity * playerRb.mass, 0));
-
-        grounded = false;
     }
 
     void OnCollisionStay()
