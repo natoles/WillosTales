5022b3a9eb13b040a2011c3b46294d362a4a83fb
Théo Bertet <38724527+TheoBertet@users.noreply.github.com>
2019-11-01 23:58:22 +0100

    Add actionnable game objects and modify moving platform (without input detection)
    Actionnable GameObjects are now available, with one mode (Reversible or not - Meaning that the player can activate or disactivate an object)
    Moving Platform can now be unique movements (Like doors, secret passages, etc) or repeated movements (Like normal moving platforms, or closing doors)
    
    Missing the Input detection in the SelectionManager.

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,44 +4,54 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public static string POS1 = "Moving to Position 1";
-    public static string POS2 = "Moving to Position 2";
-
-
-
     public Transform movingPlatform;    // Position of current objet
     public Transform position1;         // First position to reach
     public Transform position2;         // Third position to reach
     public float Speed;                 // Speed
-    private string currentState;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public bool activated = true;       // Is activated. If not, gameobject's not moving
+    public bool repeated = true;        // Is the platform moving repeatedly ?
+    public bool uniqueMove = false;     // Can the platform go back to its first position ?
+    private bool moveReached = false;   // Only for uniqueMove-mode. Activated when the movement is complete
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPosition = movingPlatform.position;
-        float distanceTravelled = (newPosition - position1.position).magnitude;
-        float totalDistance = (position1.position - position2.position).magnitude;
-
-        if(distanceTravelled >= totalDistance)
+        if (activated && !moveReached)
         {
-            Transform temp = position2;
-            position2 = position1;
-            position1 = temp;
-        }
+            Vector3 newPosition = movingPlatform.position;
+            float distanceTravelled = (newPosition - position1.position).magnitude;
+            float totalDistance = (position1.position - position2.position).magnitude;
 
-        Vector3 direction = Vector3.Normalize(position2.position - position1.position);
-        newPosition += (Vector3)(Speed * Time.deltaTime * direction);
-        movingPlatform.position = newPosition;
+            if (distanceTravelled >= totalDistance)
+            {
+                Transform temp = position2;
+                position2 = position1;
+                position1 = temp;
+
+                if(uniqueMove)
+                {
+                    reachMove();
+                }
+
+                if (!repeated)
+                {
+                    activated = !activated;
+                }
+            }
+
+            Vector3 direction = Vector3.Normalize(position2.position - position1.position);
+            newPosition += (Vector3)(Speed * Time.deltaTime * direction);
+            movingPlatform.position = newPosition;
+        }
     }
 
-    void ChangeTarget()
+    private void reachMove()
     {
+        moveReached = true;
+    }
 
+    public void activate(bool predicate)
+    {
+        activated = predicate;
     }
 }
