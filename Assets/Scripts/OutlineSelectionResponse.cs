362f79ec49cd571948fbbac1e3dc770fe122d9dd
Je5ter <yann.galan@wanadoo.fr>
2019-11-01 19:35:12 -0400

    Add outlines

---
+++ Assets/Scripts/OutlineSelectionResponse.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OutlineSelectionResponse : MonoBehaviour, ISelectionResponse
+{
+    public void OnSelect(Transform selection)
+    {
+        var outline = selection.GetComponent<Outline>();
+        if (outline != null) outline.OutlineWidth = 10;
+    }
+    
+    public void OnDeselect(Transform selection)
+    {
+        var outline = selection.GetComponent<Outline>();
+        if (outline != null) outline.OutlineWidth = 0;
+    }
+}
