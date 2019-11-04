01eb746be158b3bfc408e68e20e5db1e32084575
natoles <lindien.martin@gmail.com>
2019-11-04 14:30:34 -0500

    added pressure plate enigma

---
+++ Assets/PressurePlate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    Collider pressureZone;
+    public bool active = false;
+    public LayerMask activeLayers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressureZone = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (activeLayers == (activeLayers | (1 << col.gameObject.layer)))
+        {
+            active = true;
+        } 
+
+
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (activeLayers == (activeLayers | (1 << col.gameObject.layer)))
+        {
+            active = false;
+        }
+    }
+}
