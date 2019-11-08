412ee9eea3b4dd9496d83d38237fa5f9d439abd0
Je5ter <yann.galan@wanadoo.fr>
2019-11-07 22:54:10 -0500

    Update OutlineSelectionResponse script
    Outlines now appears only if the player is at a certain distance of the pointed object.

--- Assets/Scripts/OutlineSelectionResponse.cs
+++ Assets/Scripts/OutlineSelectionResponse.cs
@@ -2,10 +2,14 @@
 
 public class OutlineSelectionResponse : MonoBehaviour, ISelectionResponse
 {
+    [SerializeField, Range(0f, 10f)]
+    private float activationMaxDist = 5f;
+    
     public void OnSelect(Transform selection)
     {
         var outline = selection.GetComponent<Outline>();
-        if (outline != null) outline.OutlineWidth = 10;
+        if (outline != null && Vector3.Distance(Camera.main.gameObject.transform.position, selection.position) < activationMaxDist) 
+            outline.OutlineWidth = 10;
     }
     
     public void OnDeselect(Transform selection)
