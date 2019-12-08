91d96e58e2abb8ebe6b000c9108498ca3e6a7775
Je5ter <yann.galan@wanadoo.fr>
2019-12-07 21:58:49 -0500

    Add hints for the player (WIP)
    Some text can now be displayed on the screen to help the player when the Hint script is attached on a object.

---
+++ Assets/Scripts/Hint.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHint : MonoBehaviour
+{
+    public string text;
+    public Canvas hintCanvas;
+    private TextMeshProUGUI textObject;
+    private Image background;
+    private Animator anim;
+    private bool _isAlreadyActivated = false;
+
+    private void Start()
+    {
+        textObject = hintCanvas.GetComponentInChildren<TextMeshProUGUI>();
+        background = hintCanvas.GetComponentInChildren<Image>();
+        anim = hintCanvas.GetComponent<Animator>();
+    }
+
+    public void trigger()
+    {
+        if (!_isAlreadyActivated)
+        {
+            textObject.text = text;
+            hintCanvas.gameObject.SetActive(true);
+            _isAlreadyActivated = true;
+            StartCoroutine(Fade());
+        }
+    }
+    
+    public void hide()
+    {
+        hintCanvas.gameObject.SetActive(false);
+    }
+
+    IEnumerator Fade()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(()=>background.color.a==1);
+        yield return new WaitForSeconds(5f);
+        anim.SetBool("Fade", false);
+        yield return new WaitUntil(()=>background.color.a==0);
+        hide();
+        
+    }
+}
---
+++ Assets/Scripts/HintSelectionResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintSelectionResponse : MonoBehaviour, ISelectionResponse
+{
+    [SerializeField, Range(0f, 10f)] public float activationMaxDist = 5f;
+    private ShowHint hint;
+
+    public void OnSelect(Transform selection)
+    {
+        hint = selection.GetComponent<ShowHint>();
+
+        if (hint != null && Vector3.Distance(Camera.main.gameObject.transform.position, selection.position) <
+            activationMaxDist)
+        {
+            hint.trigger();
+        }
+    }
+
+    public void OnDeselect(Transform selection)
+    {
+    }
+}
